9d0a0138c3855e4a16f481623df6c48e692ed81f
VIvidh Mahajan <vividhmahajan@gmail.com>
2024-05-18 22:46:18 -0400

    work

---
+++ fontend/src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className="home">
+            <h2> ABT </h2>
+            <p>conasdadsa;</p>
+        </div>
+    );
+}
+
+export default About
---
+++ fontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h2> HOME </h2>
+            <p>conasdadsa;</p>
+        </div>
+    );
+}
+
+export default Home
